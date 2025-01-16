14febe3f3495d082f81dd5bbcca17c95fb2c6afa
Van-Tony <tony.van.professional@gmail.com>
2025-01-16 22:47:05 +0100

    Ajout des dossiers nécéssaires et fichiers .html, .css, .js du site Web

---
+++ scripts/jeu/config.js
@@ -0,0 +1,3 @@
+listeDeMots = ["Ordinateur", "Jeux", "Sport"]
+listeDePhrases = ["J'aime programmer", "J'aime l'informatique", "J'aime les jeux-vidéos"]
+
---
+++ scripts/jeu/script.js
@@ -0,0 +1,60 @@
+
+
+
+function choisirPhrasesOuMots()
+{
+    
+    choixphrases = prompt(`Choisissez entre jouer avec des "phrases" ou "mots"`)
+
+    return choixphrases
+    
+}
+
+function lancerBoucleDeJeu(choix)
+{
+    switch (choix)
+    {
+    case "phrases":
+        for (i = 0 ; i < listeDePhrases.length ; i++)
+        {
+            choix = prompt("Ecrivez la phrase : " + listeDePhrases[i])
+
+            if(choix === listeDePhrases[i])
+            {
+                
+                score++
+                
+            }
+
+        }
+    break
+
+    case "mots":
+        for (i = 0 ; i < listeDeMots.length ; i++)
+            {
+                choix = prompt("Ecrivez le mot : " + listeDeMots[i])
+                if (choix === listeDeMots[i])
+                {
+                    score++
+                }
+            }    
+    break
+    }
+}
+function afficherResultat()
+{
+    console.log("Bravo ! Votre score total est de " + score + "sur" + listeDeMots.length)
+}
+
+let score = 0
+
+function lancerJeu()
+{
+
+    let choix = choisirPhrasesOuMots()
+    lancerBoucleDeJeu(choix)
+    afficherResultat()
+}
+
+
+
